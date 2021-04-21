57cbb07861a3e508fb2b3a2e3dada25492696885
Hal Henke <halhenke@gmail.com>
2021-04-22 09:47:30 +1000

    Added `async-std` lib and got basic tabl creation going with `sqlx`

--- src/db/sqlx.rs
+++ src/db/sqlx.rs
@@ -1,47 +1,85 @@
 use sqlx::query::Query;
 use sqlx::sqlite::SqliteArguments;
-use sqlx::{Connection, Error, Executor, Pool, Sqlite, SqlitePool};
-
-
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
+use sqlx::{
+    ConnectOptions, Connection, Error, Executor, Pool, Sqlite, SqliteConnection, SqlitePool,
+};
+use std::str::FromStr;
 
 pub fn do_this() {
     println!("Hey this called");
 }
 
-pub async fn connect_db() -> Result<SqlitePool, Error> {
+const SQLITE_DB: &'static str = "sqlite://rust-git-org.sqlite";
+
+// const WORDS: &'static str = "hello rust!";
+
+pub async fn connect_db() -> Result<(), anyhow::Error> {
+    // pub async fn connect_db() -> Result<SqlitePool, anyhow::Error> {
     // let con = Connection::connect(url)
     // let conn = SQLiteConnection::connect()
-    let pool = SqlitePool::connect("sqlite::memory:").await?;
+    // let p = SqlitePool::
+    let mut conn = SqliteConnectOptions::from_str(SQLITE_DB)?
+        // let mut conn = SqliteConnectOptions::new()
+        //     .filename(SQLITE_DB)
+        .foreign_keys(true)
+        .create_if_missing(true)
+        .connect()
+        .await?;
+    // let pool = SqlitePool::connect(SQLITE_DB).await?;
     // pool.
-    pool.execute("PRAGMA foreign_keys = ON;").await?;
+    // conn.execute("PRAGMA foreign_keys = ON;").await?;
+    // pool.execute("PRAGMA foreign_keys = ON;").await?;
 
     let mkOrg = sqlx::query(
         "
-    CREATE table org (
+    CREATE table IF NOT EXISTS org (
         name TEXT NOT NULL PRIMARY KEY,
         lastrun TEXT
+    );
     ",
     );
-    pool.execute(mkOrg).await?;
+    conn.execute(mkOrg).await?;
+    // pool.execute(mkOrg).await?;
 
-    let mkRepo: Query<Sqlite, _> = sqlx::query(
+    let mkRepo = sqlx::query(
         "
-        CREATE table repo (
+        CREATE table IF NOT EXISTS repo (
+        name TEXT NOT NULL,
+        org TEXT NOT NULL,
+        created TEXT,
+        lastrun TEXT,
+        PRIMARY KEY(org, name),
+        FOREIGN KEY(org)
+            REFERENCES org (name)
+        );
+    ",
+    );
+    conn.execute(mkRepo).await?;
+    // pool.execute(mkRepo).await?;
+
+    let mkRepoQuery: Query<Sqlite, _> = sqlx::query(
+        "
+        CREATE table IF NOT EXISTS repoQuery (
             name TEXT NOT NULL,
             org TEXT NOT NULL,
-            # stars INTEGER,
-            # language TEXT,
+            stars INTEGER,
+            languages TEXT,
+            topics TEXT,
             created TEXT,
-            # updated TEXT,
+            updated TEXT,
             lastrun TEXT,
             PRIMARY KEY(org, name),
             FOREIGN KEY(org)
                 REFERENCES org (name)
-        )
+        );
     ",
     );
+    conn.execute(mkRepoQuery).await?;
+    // pool.execute(mkRepoQuery).await?;
 
     // pool.execute(sqlx::query("SELECT"));
 
-    return Ok(pool);
+    // return Ok(pool);
+    return Ok(());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // pub mod lib;
 // extern crate GithubOrgRust;
+// extern crate async;
+use async_std::task;
 
 fn main() -> Result<(), anyhow::Error> {
     println!("Hello, world!");
     // GithubOrgRust::db::sqlx::do_this();
-    GithubOrgRust::github::example::main()
+    // GithubOrgRust::github::example::main()
+    task::block_on(GithubOrgRust::db::sqlx::connect_db())
 }
