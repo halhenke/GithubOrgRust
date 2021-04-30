676435193f60bfa3277b6d926794f193c89afcbd
Hal Henke <halhenke@gmail.com>
2021-04-30 14:48:47 +1000

    Add more Functions to create DataStructures related to `Repo`s and `RepoQuery`s

--- src/types.rs
+++ src/types.rs
@@ -43,16 +43,78 @@ impl Repo {
     }
 
     pub fn repo_from_repo(repo: &OrgViewOrganizationRepositoriesEdges, org: String) -> Repo {
+        let _repo = repo.node.as_ref().unwrap();
         return Repo::new(
-            repo.node.as_ref().unwrap().name.clone(),
+            _repo.name.clone(),
             org,
-            repo.node
+            _repo.created_at.parse().expect("time did not match"),
+            Utc::now(),
+        );
+    }
+}
+
+impl RepoQuery {
+    pub fn new(
+        name: String,
+        org: String,
+        createdAt: DateTime<Utc>,
+        updatedAt: DateTime<Utc>,
+        lastrun: DateTime<Utc>,
+        topics: Vec<String>,
+        languages: Vec<String>,
+        stars: i64,
+    ) -> RepoQuery {
+        return RepoQuery {
+            name,
+            org,
+            createdAt,
+            updatedAt,
+            lastrun,
+            topics,
+            languages,
+            stars,
+        };
+    }
+    pub fn repoQuery_from_repo(
+        repo: &OrgViewOrganizationRepositoriesEdges,
+        org: String,
+    ) -> RepoQuery {
+        let _repo = repo.node.as_ref().unwrap();
+        return RepoQuery::new(
+            _repo.name.clone(),
+            org,
+            _repo.created_at.parse().expect("time did not match"),
+            _repo.updated_at.parse().expect("time did not match"),
+            Utc::now(),
+            _repo
+                .repository_topics
+                .edges
                 .as_ref()
                 .unwrap()
-                .created_at
-                .parse()
-                .expect("time did not match"),
-            Utc::now(),
+                .into_iter()
+                .map(|r| {
+                    r.as_ref()
+                        .unwrap()
+                        .node
+                        .as_ref()
+                        .unwrap()
+                        .topic
+                        .name
+                        .clone()
+                })
+                .collect(),
+            // _repo.node.as_ref().unwrap().topics,
+            _repo
+                .languages
+                .as_ref()
+                .unwrap()
+                .edges
+                .as_ref()
+                .unwrap()
+                .into_iter()
+                .map(|r| r.as_ref().unwrap().node.name.clone())
+                .collect(),
+            _repo.stargazers.total_count,
         );
     }
 }
